e80a20b6edb5c6ad92f99169c25af3346e2c5b4c
G-crown <olugbengastephen3@gmail.com>
2026-06-18 19:00:58 +0100

    Fix syntax error in page.tsx

--- app/collections/[handle]/page.tsx
+++ app/collections/[handle]/page.tsx
@@ -44,6 +44,7 @@ export default async function CollectionPage({
                     .minVariantPrice
                     .amount
                 }
+                handle={node.handle]
               />
             )
           )}

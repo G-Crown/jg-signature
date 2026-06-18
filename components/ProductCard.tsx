7cf692a9d54fd8b3b20256814d603787b6a60cbc
G-crown <olugbengastephen3@gmail.com>
2026-06-18 18:49:41 +0100

    Add link to product page in ProductCard component
    Updated ProductCard component to include a link to the product page using the handle prop.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,36 +1,47 @@
+import Link from "next/link";
 import Image from "next/image";
 
 interface ProductProps {
   title: string;
   image: string;
   price: string;
+  handle: string;
 }
 
 export default function ProductCard({
   title,
   image,
-  price
+  price,
+  handle
 }: ProductProps) {
   return (
-    <div className="group">
-      <div className="relative aspect-[3/4] overflow-hidden">
-        <Image
-          src={image}
-          alt={title}
-          fill
-          className="object-cover transition duration-700 group-hover:scale-105"
-        />
-      </div>
+    <Link href={`/products/${handle}`}>
+      <div className="group cursor-pointer">
 
-      <div className="pt-4">
-        <h3 className="heading-font text-xl">
-          {title}
-        </h3>
+        <div className="relative aspect-[3/4] overflow-hidden">
 
-        <p className="mt-2">
-          ₦{Number(price).toLocaleString()}
-        </p>
+          <Image
+            src={image}
+            alt={title}
+            fill
+            className="object-cover transition duration-700 group-hover:scale-105"
+          />
+
+        </div>
+
+        <div className="pt-4">
+
+          <h3 className="heading-font text-xl">
+            {title}
+          </h3>
+
+          <p className="mt-2">
+            ₦{Number(price).toLocaleString()}
+          </p>
+
+        </div>
+
       </div>
-    </div>
+    </Link>
   );
 }

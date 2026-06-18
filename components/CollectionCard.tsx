b07e5726b0ebdd0a5f49525c42a3dc861fc2c8f9
G-crown <olugbengastephen3@gmail.com>
2026-06-18 18:22:07 +0100

    Add CollectionCard component with link functionality

--- components/CollectionCard.tsx
+++ components/CollectionCard.tsx
@@ -1,1 +1,22 @@
+import Link from "next/link";
 
+interface Props {
+  title: string;
+  handle: string;
+}
+
+export default function CollectionCard({
+  title,
+  handle
+}: Props) {
+  return (
+    <Link
+      href={`/collections/${handle}`}
+      className="border border-neutral-200 p-10 hover:bg-black hover:text-white transition-all"
+    >
+      <h3 className="heading-font text-3xl">
+        {title}
+      </h3>
+    </Link>
+  );
+}

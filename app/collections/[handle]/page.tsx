804271dbc10e615b1999265c61fcb9f54e96391d
G-crown <olugbengastephen3@gmail.com>
2026-06-18 18:45:21 +0100

    Create CollectionPage component to display collection

---
+++ app/collections/[handle]/page.tsx
@@ -0,0 +1,57 @@
+import ProductCard
+from "@/components/ProductCard";
+
+import {
+  getCollection
+}
+from "@/lib/shopify";
+
+export default async function CollectionPage({
+  params
+}: any) {
+
+  const collection =
+    await getCollection(
+      params.handle
+    );
+
+  return (
+    <div className="pt-32">
+
+      <div className="container-custom">
+
+        <h1 className="heading-font text-6xl mb-12">
+          {
+            collection.title
+          }
+        </h1>
+
+        <div className="grid md:grid-cols-4 gap-8">
+
+          {collection.products.edges.map(
+            ({ node }: any) => (
+              <ProductCard
+                key={node.id}
+                title={node.title}
+                image={
+                  node
+                    .featuredImage
+                    ?.url
+                }
+                price={
+                  node
+                    .priceRange
+                    .minVariantPrice
+                    .amount
+                }
+              />
+            )
+          )}
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}

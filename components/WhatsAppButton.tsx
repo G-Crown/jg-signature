588f1297cab18ee0c7a8f108a5c0b28bde5f452a
G-crown <olugbengastephen3@gmail.com>
2026-06-18 18:20:02 +0100

    Use dynamic phone number in WhatsAppButton
    Updated WhatsAppButton component to use dynamic phone number from environment variable.

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { FaWhatsapp } from "react-icons/fa";
+
 export default function WhatsAppButton() {
+  const phone =
+    process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
+
   return (
     <a
-      href="https://wa.me/2347032617576"
+      href={`https://wa.me/${phone}`}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50"
     >
-      <div className="w-14 h-14 rounded-full bg-green-500 flex items-center justify-center shadow-xl">
-        WhatsApp
+      <div className="w-16 h-16 rounded-full bg-green-500 flex items-center justify-center shadow-2xl hover:scale-105 transition-all">
+        <FaWhatsapp
+          className="text-white"
+          size={34}
+        />
       </div>
     </a>
   );
